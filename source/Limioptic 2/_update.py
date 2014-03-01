7ffe9c7040c26241a1c6b772e070c7a0dddce2bc
Alexander Stolz <kontakt@amstolz.de>
2014-03-01 13:25:03 +0100

    fix:  update for precompiled limioptic

---
+++ source/Limioptic 2/_update.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import urllib as ul
+import sys
+
+print "reading file list.."
+a = ul.urlopen("http://ams.amstolz.de/update.txt")
+b = a.readlines()
+a.close()
+
+q = raw_input("press ENTER to start update process. press \"q\" to cancel..  ")
+
+if sys.argv[0].endswith(".py"):
+	if (q != "q"):
+	    for i in range(1, len(b)):
+	        ul.urlretrieve(b[i].split()[0], b[i].split()[1])
+	        print "{} updated".format(b[i].split()[1])
+	    print "\n",b[0]
+
+	    print "update complete."
+	    raw_input("press enter to exit")
+else:
+	print "precompiled version detected"
+	try:
+		sys.winver
+		print "windows os detected"
+		ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/blob/master/precompiled%20for%20windows/___LIMIOPTIC.exe", "___LIMIOPTIC.exe")
+		print "___LIMIOPTIC.exe updated"
+		ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/blob/master/precompiled%20for%20windows/liblimioptic-win.so", "liblimioptic-win.so")
+		print "liblimioptic-win updated"
+	except:
+		print "linux os detected"
+		ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/blob/master/precompiled%20for%20windows/___LIMIOPTIC.exe", "___LIMIOPTIC.exe")
+		print "___LIMIOPTIC.exe updated"
+		ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/blob/master/precompiled%20for%20windows/liblimioptic-linux.so", "liblimioptic-linux.so")
+		print "liblimioptic-linux updated"
--- source/Limioptic 2/setup.py
+++ source/Limioptic 2/setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 import matplotlib
 import shutil
 import os
+import sys
 
 setup( 
       author="Alexander Stolz",
-      console=["setup_helper.py"],
+      console=["setup_helper.py", "_update.py"],
       py_modules=["___RUN__LIMIOPTIC_2", "ams_spicker", "beamprofile", "importsrc", "limioptic", "syntax", "update"],
       data_files=matplotlib.get_py2exe_datafiles(),
       options={'py2exe': { 'includes' : ["matplotlib.backends.backend_tkagg", "pyqtgraph", "vtk"], 'excludes': ['_gtkagg', '_tkagg'], "bundle_files" : 3}},
@@ -19,4 +20,13 @@ for module in ext_modules:
 	except:
 		pass
 
-os.rename(os.getcwd() + "/dist/setup_helper.exe", os.getcwd() + "/dist/___LIMIOPTIC.exe")
+try:
+	sys.winver
+	thisos = "windows"
+except:
+	thisos = "linux"
+
+cwd = os.getcwd()
+os.rename(cwd + "/dist/setup_helper.exe", cwd + "/dist/___LIMIOPTIC.exe")
+shutil.move(cwd + "/dist", cwd.replace("\\", "/").rsplit("/", 1)[0].rsplit("/", 1)[0] + "/precompiled for {}".format(thisos))
+shutil.rmtree(cwd + "/build")
