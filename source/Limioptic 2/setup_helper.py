92ac8721228ee86c24e09313d55c2fd17ebfcb21
alexander-stolz <amstolz@gmail.com>
2014-03-24 13:57:37 +0100

    fix:  precompiled version under win7

--- source/Limioptic 2/setup_helper.py
+++ source/Limioptic 2/setup_helper.py
@@ -2,5 +2,6 @@ def explicit_imports_for_py2exe():
     from scipy.sparse.csgraph import _validation
     from pyqtgraph.graphicsItems import TextItem
     from pyqtgraph import setConfigOption
+    import scipy.special._ufuncs_cxx
 
 import ___RUN__LIMIOPTIC_2
