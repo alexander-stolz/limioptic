d3ecda52c21193a38a974cd30fcbed07fdc4ab2b
alexander-stolz <amstolz@gmail.com>
2014-03-26 13:53:22 +0100

    Update _update.py

--- source/Limioptic 2/_update.py
+++ source/Limioptic 2/_update.py
@@ -2,6 +2,7 @@
 
 import urllib as ul
 import sys
+import os
 
 print "reading file list.."
 a = ul.urlopen("http://ams.amstolz.de/update.txt")
@@ -16,21 +17,16 @@ if sys.argv[0].endswith(".py"):
 	        ul.urlretrieve(b[i].split()[0], b[i].split()[1])
 	        print "{} updated".format(b[i].split()[1])
 	    print "\n",b[0]
+	try:
+		os.remove("liblimioptic-linux.so")
+	except:
+		pass
 else:
 	print "precompiled version detected"
-	try:
-		sys.winver
-		print "windows os detected"
-		ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/raw/master/precompiled%20for%20windows/___LIMIOPTIC.exe", "___LIMIOPTIC.exe")
-		print "___LIMIOPTIC.exe updated"
-		ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/raw/master/precompiled%20for%20windows/liblimioptic-win.so", "liblimioptic-win.so")
-		print "liblimioptic-win updated"
-	except:
-		print "linux os detected"
-		ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/raw/master/precompiled%20for%20windows/___LIMIOPTIC.exe", "___LIMIOPTIC.exe")
-		print "___LIMIOPTIC.exe updated"
-		ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/raw/master/precompiled%20for%20windows/liblimioptic-linux.so", "liblimioptic-linux.so")
-		print "liblimioptic-linux updated"
+	ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/raw/master/precompiled%20for%20windows/___LIMIOPTIC.exe", "___LIMIOPTIC.exe")
+	print "___LIMIOPTIC.exe updated"
+	ul.urlretrieve(r"https://github.com/alexander-stolz/limioptic/raw/master/precompiled%20for%20windows/liblimioptic-win.so", "liblimioptic-win.so")
+	print "liblimioptic-win updated"
 
 print "update complete."
 raw_input("press enter to exit")
