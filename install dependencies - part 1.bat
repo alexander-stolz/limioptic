cacf82ae875b468004a8ef0e2e07b734a019b5ba
Alexander Stolz <amstolz@gmail.com>
2017-06-14 14:01:08 +0200

    installation

--- install dependencies - part 1.bat
+++ install dependencies - part 1.bat
@@ -1,4 +1,8 @@
 cd setup_files
+@echo.
+@echo Please install python to the default directory C:\Python27. Otherwise you have to change the windows path environment variable accordingly.
+@echo.
+@PAUSE
 python-2.7.13.msi
 setx path "%PATH%;C:\Python27"
 
