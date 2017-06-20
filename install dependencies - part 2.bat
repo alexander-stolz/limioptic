83e40fadd19436dba1c3f8c9df2460a843536a75
Alexander Stolz <amstolz@gmail.com>
2017-06-20 14:30:10 +0200

    added vtk to installation

--- install dependencies - part 2.bat
+++ install dependencies - part 2.bat
@@ -5,5 +5,6 @@ python -m pip install scipy-0.19.0-cp27-cp27m-win32.whl
 python -m pip install PyQt4-4.11.4-cp27-cp27m-win32.whl
 python -m pip install matplotlib
 python -m pip install pyqtgraph
+python -m pip install VTK-5.10.1+qt486-cp27-none-win32.whl
 
 PAUSE
