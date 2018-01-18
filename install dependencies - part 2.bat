3034ac919839f0b32d4c4b06a6a20550ec85850b
Alexander M. Stolz <amstolz@gmail.com>
2018-01-18 16:17:24 +0100

    Update install dependencies - part 2.bat

--- install dependencies - part 2.bat
+++ install dependencies - part 2.bat
@@ -1,10 +1,10 @@
 cd setup_files
 python get-pip.py
-python -m pip install setup-files/numpy-1.13.0+mkl-cp27-cp27m-win32.whl
-python -m pip install setup-files/scipy-0.19.0-cp27-cp27m-win32.whl
-python -m pip install setup-files/PyQt4-4.11.4-cp27-cp27m-win32.whl
+python -m pip install setup_files/numpy-1.13.0+mkl-cp27-cp27m-win32.whl
+python -m pip install setup_files/scipy-0.19.0-cp27-cp27m-win32.whl
+python -m pip install setup_files/PyQt4-4.11.4-cp27-cp27m-win32.whl
 python -m pip install matplotlib
 python -m pip install pyqtgraph
-python -m pip install setup-files/VTK-5.10.1+qt486-cp27-none-win32.whl
+python -m pip install setup_files/VTK-5.10.1+qt486-cp27-none-win32.whl
 
 PAUSE
