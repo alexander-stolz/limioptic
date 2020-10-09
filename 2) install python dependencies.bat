0613c7e7c64b388039ecf47f3f205764f657c932
Dr. Alexander M. Stolz <amstolz@gmail.com>
2020-10-09 19:11:35 +0200

    removed vtk

--- 2) install python dependencies.bat
+++ 2) install python dependencies.bat
@@ -3,6 +3,5 @@ python -m pip install scipy
 python -m pip install pyqt5
 python -m pip install matplotlib
 python -m pip install pyqtgraph
-python -m pip install https://download.lfd.uci.edu/pythonlibs/w3jqiv8s/VTK-8.2.0-cp38-cp38-win32.whl
 
 PAUSE
